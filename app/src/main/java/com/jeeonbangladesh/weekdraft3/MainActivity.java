3e4acccd6516a953b7f7c0b88a26108dc7b97999
Syed Adiba Shuhin <adibashuhin@gmail.com>
2018-12-17 19:09:52 +0600

    Add files via upload
    Toast works here.

--- app/src/main/java/com/jeeonbangladesh/weekdraft3/MainActivity.java
+++ app/src/main/java/com/jeeonbangladesh/weekdraft3/MainActivity.java
@@ -10,11 +10,15 @@ import android.widget.Toast;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import org.threeten.bp.DayOfWeek;
+import org.threeten.bp.Month;
+import org.threeten.bp.Year;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +26,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MaterialCalendarView materialCalendarView =(MaterialCalendarView) findViewById(R.id.calendarView);
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
         materialCalendarView.state().edit()
                 .setFirstDayOfWeek(DayOfWeek.of(Calendar.MONDAY))
                 .setMinimumDate(CalendarDay.from(2018, 1, 1))
                 .setMaximumDate(CalendarDay.from(2020, 2, 2))
                 .setCalendarDisplayMode(CalendarMode.WEEKS)
                 .commit();
-        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+
+        materialCalendarView.setOnDateLongClickListener(new OnDateLongClickListener() {
             @Override
-            public void onDateSelected(@NonNull MaterialCalendarView materialCalendarView, @NonNull CalendarDay calendarDay, boolean b) {
-                Toast.makeText(MainActivity.this,"" ,Toast.LENGTH_SHORT).show();
+            public void onDateLongClick(@NonNull MaterialCalendarView materialCalendarView, @NonNull CalendarDay calendarDay) {
+                Toast.makeText(MainActivity.this, "" + calendarDay , Toast.LENGTH_LONG).show();
             }
-        });
+        } );
+
         final Button button= findViewById(R.id.add_calendar_event);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
